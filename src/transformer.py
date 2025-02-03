138c14a1ae100693b81c3ec8b69041fb8584cb16
fefi224 <fefi224@gmail.com>
2025-02-03 14:20:00 +0700

    implement full encoder-decoder transformer

--- src/transformer.py
+++ src/transformer.py
@@ -35,3 +35,35 @@ class DecoderLayer(nn.Module):
         a2, _ = self.cross_attn(x, enc_out, enc_out, src_mask)
         x = self.norm2(x + self.drop(a2))
         return self.norm3(x + self.ff(x))
+
+class Transformer(nn.Module):
+    def __init__(self, vocab_size, d_model=512, n_heads=8, n_layers=6, d_ff=2048, dropout=0.1, max_len=512):
+        super().__init__()
+        from src.positional_encoding import PositionalEncoding
+        self.embed = nn.Embedding(vocab_size, d_model, padding_idx=0)
+        self.pos = PositionalEncoding(d_model, max_len, dropout)
+        self.encoder = nn.ModuleList([EncoderLayer(d_model, n_heads, d_ff, dropout) for _ in range(n_layers)])
+        self.decoder = nn.ModuleList([DecoderLayer(d_model, n_heads, d_ff, dropout) for _ in range(n_layers)])
+        self.fc_out = nn.Linear(d_model, vocab_size)
+        self._init_weights()
+
+    def _init_weights(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+
+    def encode(self, src, src_mask=None):
+        x = self.pos(self.embed(src))
+        for layer in self.encoder:
+            x = layer(x, src_mask)
+        return x
+
+    def decode(self, tgt, enc_out, src_mask=None, tgt_mask=None):
+        x = self.pos(self.embed(tgt))
+        for layer in self.decoder:
+            x = layer(x, enc_out, src_mask, tgt_mask)
+        return self.fc_out(x)
+
+    def forward(self, src, tgt, src_mask=None, tgt_mask=None):
+        enc = self.encode(src, src_mask)
+        return self.decode(tgt, enc, src_mask, tgt_mask)
